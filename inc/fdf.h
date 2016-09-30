2fe6dcafc37e793e04c57f4909c75a93246220b8
wPef <f.demoncade@gmail.com>
2016-09-30 20:01:04 +0200

    started list, NEEDED memory allocation

--- inc/fdf.h
+++ inc/fdf.h
@@ -4,37 +4,49 @@
 # include "mlx.h"
 # define ERROR(ID) (ft_strcmp(ID, id) == 0 ? 1 : 0)
 # define WHITE 0x00FFFFFF
+# define Y_MAX all->y_max
+# define X_MAX all->x_max
 # define BUFF_SIZE 50
 
 typedef struct	s_fdf
 {
 	int		**tab;
+	t_dot	**dots;
 	int		x_max;
 	int		y_max;
 	void	*mlx;
 	void	*win;
 }				t_fdf;
 
-int	usage(char *id);
-int	error(char *id);
+typedef struct	s_dot;
+{
+	int		x;
+	int		y;
+	t_dot	*next;
+}				t_dot;
 
-int	check_file(char **av, t_fdf *all);
-int	read_file1(int fd);
-int	check_line1(char *line);
-int	read_file2(int fd);
-int	check_line2(char *line);
+int				usage(char *id);
+int				error(char *id);
 
-int **make_tab(int fd, int y_max, int x_max);
-int	*make_line(int fd, int x_max);
-char	**splitmap(char *line, int x_max);
+int				check_file(char **av, t_fdf *all);
+int				read_file1(int fd);
+int				check_line1(char *line);
+int				read_file2(int fd);
+int				check_line2(char *line);
 
-int	get_infos(char *file, t_fdf *all);
+int				**make_tab(int fd, int y_max, int x_max);
+int				*make_line(int fd, int x_max);
+char			**splitmap(char *line, int x_max);
 
-int run_mlx(t_fdf *all);
-void	print_fdf(t_fdf *all);
-void	print_line(int *line, int y, t_fdf *all);
+int				get_infos(char *file, t_fdf *all);
+t_dot			**get_dots (t_fdf *all);
+t_dot			*init_list (t_fdf *all);
 
-void	affiche_tab_debug(int **tab, int y_max, int x_max);
-void	debug_affmap(char **tab);
+int				run_mlx(t_fdf *all);
+void			print_fdf(t_fdf *all);
+void			print_line(int *line, int y, t_fdf *all);
+
+void			affiche_tab_debug(int **tab, int y_max, int x_max);
+void			debug_affmap(char **tab);
 
 #endif
--- src/get_infos.c
+++ src/get_infos.c
@@ -1,6 +1,6 @@
 #include "fdf.h"
 
-int	get_infos(char *file, t_fdf *all)
+int		get_infos(char *file, t_fdf *all)
 {
 	int	fd;
 
@@ -8,5 +8,42 @@ int	get_infos(char *file, t_fdf *all)
 		return (error("Structure all == NULL"));
 	fd = open(file, O_RDONLY);
 	all->tab = make_tab(fd, all->y_max, all->x_max);
+	all->dots = get_dots(all);
 	return (all->tab ? 1 : error("TAB NOT CREATED"));
+}
+
+t_dot	**get_dots (t_fdf *all)
+{
+	t_dot	**ret;
+	t_dot	*ptr;
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 0;
+
+	&ret = init_list(all);
+	ptr = *ret;
+	ptr = ptr->next;
+	while (y < Y_MAX)
+	{
+		while (x < X_MAX)
+		{
+			make_node(x, y, );
+			x++;
+		}
+		y++;
+	}
+}
+
+t_dot *init_list (t_fdf *all)
+{
+	t_dot *ret;
+
+	if (ret = malloc(sizeof(t_dot)) == NULL)
+		return (error("Memory allocation failed !"));
+	ret->x = get_pixelpos_x(0, 0, all->tab[0][0]);
+	ret->y = get_pixelpos_y(0, 0, all->tab[0][0]);
+	ret->next = NULL;
+	return (ret);
 }
